c69300eb45835ef0fee4da68ca21dadf1777b41c
Juan Jose <jjsanchezv@ufpso.edu.co>
2025-09-04 23:59:58 -0500

    Ejemplo1

---
+++ src/Ejemplo1.java
@@ -0,0 +1,18 @@
+public class Ejemplo1 {
+    public static void main(String[] args) throws Exception {
+        int edad = 27;
+        float altura = 1.78f;
+        String primerNombre = "Camilo";
+        boolean mayorEdad = false;
+
+        System.out.println("Su edad es " + edad);
+        System.out.println("Su altura es " + altura);
+        System.out.println("Su primer nombre es " + primerNombre);
+        System.out.println("Es mayor de edad? " + mayorEdad);
+
+        
+
+
+
+    }
+}
